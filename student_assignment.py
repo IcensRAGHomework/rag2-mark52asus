b6e5d3fa78ccf0ae2f068f0d01156f66a91ede95
mark52asus <mark52asus@gmail.com>
2025-01-26 10:47:06 +0800

    hw 0202 0126-5

--- student_assignment.py
+++ student_assignment.py
@@ -41,7 +41,7 @@ def hw02_2(q2_pdf):
 
     # 使用 RecursiveCharacterTextSplitter 配置針對章節分割
     text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=55,  # 每個 chunk 的最大字數
+        chunk_size=52,  # 每個 chunk 的最大字數
         chunk_overlap=0,  # 無重疊
         separators=[
             r"\n.*第.*章.*?\n",  # 僅分割章節
